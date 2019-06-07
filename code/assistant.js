ce6a3e6dcf78f0ad076cfd08c4516771daefec5a
alexiscruzdavid <45520789+alexiscruzdavid@users.noreply.github.com>
2019-06-07 15:30:28 -0400

    Update assistant.js

--- code/assistant.js
+++ code/assistant.js
@@ -37,26 +37,10 @@ switch(topic){
         // * On Thermostat call
         case thermostat:
             try{
-                //raise the temperature to a certain number
-                if(message.slots[0].value.value === 'raise' && message.slots[1].value.kind === 'Number')
-                {
-                    relay.makeTemp(message.slots[1].value.value);
-                    relay.stopWaiting();
-                    console.log(message.slots[0]);
-                    console.log('thermos on');
-                }
-                //cool the temperature to a certain number
-                else if(message.slots[0].value.value === 'cool' && message.slots[1].value.kind === 'Number')
-                {
-                    relay.makeTemp(message.slots[1].value.value);
-                    relay.stopWaiting();
-                    console.log('thermos on');
-                }
                 //tell the temperature
-                else if(message.slots[0].value.value === 'read')
+                if(message.slots[0].value.value === 'read')
                 {
                     relay.tellTemp();
-                    console.log(message.slots[0]);
                     //respond with the temperature
                     client.snipsRespond({
                         sessionId: message.sessionId,
@@ -64,18 +48,15 @@ switch(topic){
                     });
                     relay.sleep(3000);
                     relay.stopWaiting();
-                    console.log('thermos on');
                 }
                 //set the temperature to a certain number
                 else if(message.slots[0].value.value === 'set'  && message.slots[1].value.kind === 'Number')
                 {
-                    relay.makeTemp(message.slots[1].value.value);
+                    relay.makeTemp(message.slots[1].value.value-1,message.slots[1].value.value+1);
                     relay.stopWaiting();
-                    console.log('thermos on');
                 }
                 else{
                     relay.stopWaiting();
-                    console.log('thermos off');
                 }
             }
             // Expect error if nothing is understood
